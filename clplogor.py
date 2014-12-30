5ce07b7767e255521c71ea130b750e32b9205a7b
owlran <owlran1088@gmail.com>
2014-12-30 14:27:47 +0800

    update

--- clplogor.py
+++
@@ -1,9 +0,0 @@
-import argparse
-
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-s", "--string", help="echo the string", default="Hello World!!")
-    args = parser.parse_args()
-    print args.string
-if __name__ == '__main__':
-    main()
