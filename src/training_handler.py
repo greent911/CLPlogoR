e47b10d2f02bba5b1174e750300dddd86e1fcaf4
greent911 <greent911@gmail.com>
2015-01-02 12:59:41 +0800

    avoid degenerated triangles

--- src/training_handler.py
+++ src/training_handler.py
@@ -13,6 +13,11 @@ class TrainingHandler():
         self.flann = cv2.FlannBasedMatcher(index_params,search_params)
         self.DISTCOMPAREFACTOR = 0.7
         self.SIM_THRESHOLD = 0.95
+        self.TRIANGLE_CONSTRAINT_DIST = 5.0
+        self.TRIANGLE_CONSTRAINT_ANGLE = 15.0
+        self.TRIANGLE_CONSTRAINT_ECCENTRICITY_LOWERBOUND = 1.0/3
+        self.TRIANGLE_CONSTRAINT_ECCENTRICITY_UPPERBOUND = 3.0
+        self.triangleSet = []
         
     def drawKeyPoints(self, img1, img2, keypoints1, keypoints2, num=-1):
         h1, w1 = img1.shape[:2]
@@ -99,8 +104,7 @@ class TrainingHandler():
                     
         return e_match,list(v_match)
 
-    def create_triangles(self,edge_matches,v_matches,keypoints):
-        triangles = []
+    def create_triangles(self,edge_matches,v_matches,keypoints,descriptors):
         triangles_num = list(itertools.combinations(v_matches,3))
         for keyindexi,keyindexj,keyindexk in triangles_num:
             # print keyindexi,keyindexj,keyindexk
@@ -116,25 +120,25 @@ class TrainingHandler():
             vijy = float(jy - iy)
             length_vik = sqrt(abs(vikx*vikx+viky*viky))
             length_vij = sqrt(abs(vijx*vijx+vijy*vijy))
-            if length_vik == 0:
-                length_vik=1
-            if length_vij == 0:
-                length_vij=1
+            if length_vik < self.TRIANGLE_CONSTRAINT_DIST or length_vij < self.TRIANGLE_CONSTRAINT_DIST or length_vij/length_vik < self.TRIANGLE_CONSTRAINT_ECCENTRICITY_LOWERBOUND or length_vij/length_vik > self.TRIANGLE_CONSTRAINT_ECCENTRICITY_UPPERBOUND:
+                continue
             vcos = (vikx*vijx+viky*vijy)/length_vik/length_vij
             delta1 = degrees(acos(round(vcos,13)))
+            if delta1 < self.TRIANGLE_CONSTRAINT_ANGLE:
+                continue
             vjkx = float(kx - jx)
             vjky = float(ky - jy)
             vjix = -vijx
             vjiy = -vijy
             length_vjk = sqrt(abs(vjkx*vjkx+vjky*vjky))
             length_vji = sqrt(abs(vjix*vjix+vjiy*vjiy))
-            if length_vjk == 0:
-                length_vjk=1
-            if length_vji == 0:
-                length_vji=1
+            if length_vjk < self.TRIANGLE_CONSTRAINT_DIST or length_vji < self.TRIANGLE_CONSTRAINT_DIST or length_vjk/length_vji < self.TRIANGLE_CONSTRAINT_ECCENTRICITY_LOWERBOUND or length_vjk/length_vji > self.TRIANGLE_CONSTRAINT_ECCENTRICITY_UPPERBOUND:
+                continue
             vcos = (vjkx*vjix+vjky*vjiy)/length_vjk/length_vji
             delta2 = degrees(acos(round(vcos,13)))
-            triangles.append([keypoints[keyindexi],keypoints[keyindexj],keypoints[keyindexk],delta1,delta2,edge_matches[keyindexi,keyindexj],edge_matches[keyindexj,keyindexk],edge_matches[keyindexk,keyindexi]])
+            if delta2 < self.TRIANGLE_CONSTRAINT_ANGLE:
+                continue
+            self.triangleSet.append([descriptors[keyindexi],descriptors[keyindexj],descriptors[keyindexk],delta1,delta2,edge_matches[keyindexi,keyindexj],edge_matches[keyindexj,keyindexk],edge_matches[keyindexk,keyindexi]])
 
     def feature_matching(self, img1path, img2path):
         """Feature Matching
@@ -149,7 +153,6 @@ class TrainingHandler():
         kp1, des1 = sift.detectAndCompute(img1,None)
         kp2, des2 = sift.detectAndCompute(img2,None)
 
-        
         # Need only good matches
         matches = self.flann.knnMatch(des1,des2,k=2)
         goodmatches = []
@@ -161,17 +164,16 @@ class TrainingHandler():
         # print goodmatches[0].distance
         # print matches[233][0].distance
         
-        # Sort goodmatches by distance
         indices = range(len(goodmatches))
-        indices.sort(key=lambda i: goodmatches[i].distance)
+        # indices.sort(key=lambda i: goodmatches[i].distance)
 
         goodkeypoints1 = []
+        goodkeypoints2 = []
+        goodkeydes1 = []
         for i in indices:
             goodkeypoints1.append(kp1[goodmatches[i].queryIdx])
-        goodkeypoints2 = []
-        for i in indices:
             goodkeypoints2.append(kp2[goodmatches[i].trainIdx])
-            # print kp2[goodmatches[i].trainIdx].angle
+            goodkeydes1.append(des1[goodmatches[i].queryIdx])
             # print kp2[goodmatches[i].trainIdx].pt
         # test = cv2.drawKeypoints(img2,[goodkeypoints2[47]],flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
         # test = cv2.drawKeypoints(img2,[goodkeypoints2[11]],flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
@@ -182,8 +184,9 @@ class TrainingHandler():
         # cv2.destroyAllWindows()
         # self.drawKeyPoints(img1,img2,goodkeypoints1,goodkeypoints2)
         ematch,vmatch = self.similarity_keypoints(goodkeypoints1,goodkeypoints2)
-        self.create_triangles(ematch,vmatch,goodkeypoints1)
+        self.create_triangles(ematch,vmatch,goodkeypoints1,goodkeydes1)
 
 if __name__ == '__main__':
    trHandler = TrainingHandler()
    trHandler.feature_matching('box.png','box_in_scene.png')
+   print len(trHandler.triangleSet)
