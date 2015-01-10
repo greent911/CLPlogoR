90841e294fe1d89a5a6567426ecafe471032506b
owlran <owlran1088@gmail.com>
2015-01-10 22:37:52 +0800

    add test function and modified validate()

--- src/main.py
+++ src/main.py
@@ -24,27 +24,105 @@ def validate(logo_classes):
     # we need to check number of match triangle in same logo class images
     # and no-logo images.
 
-    for logo_name in logo_classes[:1]:
-        print logo_name
-        recognizer = Recognizer()
+    model_list = os.listdir('../model/')
+    model_list = [model for model in model_list if 'DS' not in model]
+    model_list = [model.replace('.pkl','') for model in model_list]
 
-        if 'no-logo' in logo_name:
+    for logo_name in logo_classes:
+        if logo_name not in model_list:
             continue
         else:
-            trHandler = TrainingHandler(logo_name)
-            imgPaths = getImagePath.getImagePath(logo_name,2)
+            print logo_name
+            recognizer = Recognizer()
 
-            for imgPath in imgPaths[:1]:
-                print imgPath
-                recognizer.recognize(imgPath, trHandler)
+            if 'no-logo' in logo_name:
+                continue
+            else:
+                trHandler = TrainingHandler(logo_name)
+                imgPaths = getImagePath.getImagePath(logo_name,2)
 
-            nologoImgPaths = getImagePath.getImagePath('no-logo',2)
+                for imgPath in imgPaths:
+                    print imgPath
+                    recognizer.recognize(imgPath, trHandler)
 
-            for imgPath in nologoImgPaths[:1]:
-                print imgPath
-                recognizer.recognize(imgPath, trHandler)
+                nologoImgPaths = getImagePath.getImagePath('no-logo',2)
+
+                for imgPath in nologoImgPaths[:30]:
+                    print imgPath
+                    recognizer.recognize(imgPath, trHandler)
 
             recognizer.writeImgTriangleCounter(logo_name)
+
+def test(logo_classes):
+
+    f = open('../test_result', 'w')
+
+    model_list = os.listdir('../model/')
+    model_list = [model for model in model_list if 'DS' not in model]
+    model_list_without_pkl = [model.replace('.pkl','') for model in model_list]
+
+    for model in model_list_without_pkl:
+
+        trHandler = TrainingHandler(model)
+        recognizer = Recognizer()
+
+        for logo_name in model_list_without_pkl:
+            imgPaths = getImagePath.getImagePath(logo_name, 3)
+
+            for imgPath in imgPaths[:1]:
+                recognizer.recognize(imgPath, trHandler)
+
+                f = open('../test_result', 'a')
+                print '-------------'
+                print 'model: %s' % model
+                print recognizer.triangleMatchCount()
+                count = recognizer.triangleMatchCount()
+                print '-------------'
+                f.write(imgPath + ' ' + logo_name + ' ' + model + ' ' + str(count) + '\n')
+                f.close()
+
+        imgPaths = getImagePath.getImagePath('no-logo', 3)
+        for imgPath in imgPaths[:1]:
+            recognizer.recognize(imgPath, trHandler)
+
+            f = open('../test_result', 'a')
+            print '-------------'
+            print 'model: %s' % model
+            print recognizer.triangleMatchCount()
+            count = recognizer.triangleMatchCount()
+            print '-------------'
+            f.write(imgPath + ' ' + logo_name + ' ' + model + ' ' + str(count) + '\n')
+
+#    for logo_name  in logo_classes:
+#        print logo_name
+#        recognizer = Recognizer()
+#
+#        if logo_name not in model_list_without_pkl:
+#            continue
+#
+#        if 'no-logo' in logo_name:
+#            imgPaths = getImagePath.getImagePath(logo_name,3)
+#            imgPaths = imgPaths[:30]
+#        else:
+#            imgPaths = getImagePath.getImagePath(logo_name,3)
+#
+#
+#        for imgPath in imgPaths[:1]:
+#            print imgPath
+#            for model in model_list:
+#                model_name = model.replace('.pkl', '')
+#                trHandler = TrainingHandler(model_name)
+#                recognizer = Recognizer()
+#                recognizer.recognize(imgPath, trHandler)
+#
+#                f = open('../test_result', 'a')
+#                print '-------------'
+#                print 'model: %s' % model
+#                print recognizer.triangleMatchCount()
+#                count = recognizer.triangleMatchCount()
+#                print '-------------'
+#                f.write(imgPath + ' ' + logo_name + ' ' + model + ' ' + str(count) + '\n')
+#                f.close()
 
 if __name__=='__main__':
 
@@ -58,5 +136,5 @@ if __name__=='__main__':
     getImagePath = GetImagePath(flickr_db_path)
 
     #train(logo_classes)
-    validate(logo_classes)
-
+    #validate(logo_classes)
+    test(logo_classes)
